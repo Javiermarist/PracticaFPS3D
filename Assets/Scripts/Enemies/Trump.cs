442ec9ffb7cc7b37ea2f31f88b1c5b50c8227bb8
Javiermarist <javiermarist@gmail.com>
2025-02-04 22:29:35 +0100

    Trump looks at Player

--- Assets/Scripts/Enemies/Trump.cs
+++ Assets/Scripts/Enemies/Trump.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 public class Trump : MonoBehaviour
 {
     [SerializeField] private float hp;
+    
+    private PlayerController playerController;
+    [SerializeField] private Animator animator; // Reference to the Animator component
+    
+    private void Start()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+    }
+
+    private void Update()
+    {
+        if (playerController != null)
+        {
+            Vector3 direction = playerController.transform.position - transform.position;
+            direction.y = 0;
+            transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+        }
+    }
 
     private void OnCollisionEnter(Collision other)
     {
@@ -24,5 +42,13 @@ public class Trump : MonoBehaviour
         {
             Die();
         }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Trump died");
+        animator.SetTrigger("Die");
+        GetComponentInChildren<MeshRenderer>().enabled = false;
+        Destroy(gameObject, animator.GetCurrentAnimatorStateInfo(0).length);
     }
 }
--- Assets/Scripts/Player/Bullet/Bullet.cs
+++ Assets/Scripts/Player/Bullet/Bullet.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public float damage;
+
     void OnCollisionEnter(Collision collision)
     {
         Destroy(gameObject);
